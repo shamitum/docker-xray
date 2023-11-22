e2c92b5354057d537af92e35d21e24bf965bd7dc
D S I GUNASEKARA <148281527+shamitum@users.noreply.github.com>
2023-11-22 23:27:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,25 +16,172 @@ then
 
     cat > ./config.json <<EOF
 {
-    "inbounds": [{
-        "port": ${PORT},
-        "protocol": "vless",
-        "settings": {
-            "clients": [{
-                "id": "${ID}"
-            }],
-            "decryption":"none"
+  "api": {
+    "services": [
+      "HandlerService",
+      "LoggerService",
+      "StatsService"
+    ],
+    "tag": "api"
+  },
+  "dns": null,
+  "fakeDns": null,
+  "inbounds": [
+    {
+      "listen": "127.0.0.1",
+      "port": 62789,
+      "protocol": "dokodemo-door",
+      "settings": {
+        "address": "127.0.0.1"
+      },
+      "sniffing": null,
+      "streamSettings": null,
+      "tag": "api"
+    },
+    {
+      "listen": null,
+      "port": 2083,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "email": "",
+            "flow": "",
+            "id": "${ID}"
+          }
+        ],
+        "decryption": "none",
+        "fallbacks": []
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls",
+          "quic",
+          "fakedns"
+        ],
+        "enabled": true
+      },
+      "streamSettings": {
+        "grpcSettings": {
+          "multiMode": true,
+          "serviceName": ""
         },
-        "streamSettings": {
-            "network": "ws",
-            "wsSettings": {
-                "path": "${WSPATH}"
-            }
+        "network": "grpc",
+        "realitySettings": {
+          "dest": "${SNI}:443",
+          "maxClient": "",
+          "maxTimediff": 0,
+          "minClient": "",
+          "privateKey": "yDhrleT3qqAjN1a85pE3vv7EAytGIjIlMRyAsN8skF8",
+          "serverNames": [
+            "${SNI}"
+          ],
+          "settings": {
+            "fingerprint": "chrome",
+            "publicKey": "7SeUnjm4sDSlop0CwJO1el8VswduF0hO1GlAyYaIgE0",
+            "serverName": "",
+            "spiderX": "/"
+          },
+          "shortIds": [
+            "90ae3e6a"
+          ],
+          "show": false,
+          "xver": 0
+        },
+        "security": "reality"
+      },
+      "tag": "inbound-2083"
+    },
+    {
+      "listen": null,
+      "port": 8080,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "email": "",
+            "id": "${ID}"
+          }
+        ]
+      },
+      "sniffing": {
+        "destOverride": [
+          "http",
+          "tls",
+          "quic",
+          "fakedns"
+        ],
+        "enabled": true
+      },
+      "streamSettings": {
+        "network": "ws",
+        "security": "none",
+        "wsSettings": {
+          "acceptProxyProtocol": false,
+          "headers": {},
+          "path": "/"
         }
-    }],
-    "outbounds": [{
-        "protocol": "freedom"
-    }]
+      },
+      "tag": "inbound-8080"
+    }
+  ],
+  "log": {
+    "error": "./error.log",
+    "loglevel": "warning"
+  },
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "policy": {
+    "levels": {
+      "0": {
+        "statsUserDownlink": true,
+        "statsUserUplink": true
+      }
+    },
+    "system": {
+      "statsInboundDownlink": true,
+      "statsInboundUplink": true
+    }
+  },
+  "reverse": null,
+  "routing": {
+    "domainStrategy": "IPIfNonMatch",
+    "rules": [
+      {
+        "inboundTag": [
+          "api"
+        ],
+        "outboundTag": "api",
+        "type": "field"
+      },
+      {
+        "ip": [
+          "geoip:private"
+        ],
+        "outboundTag": "blocked",
+        "type": "field"
+      },
+      {
+        "outboundTag": "blocked",
+        "protocol": [
+          "bittorrent"
+        ],
+        "type": "field"
+      }
+    ]
+  },
+  "stats": {},
+  "transport": null
 }
 EOF
 else
